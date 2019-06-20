d8b5537bd3ecd09fb748df71e1453db6abe0dede
Gabriel BOBARD <gbobard@gmail.com>
2019-06-20 10:30:14 +0100

    eshop_master v2 php7.3 CI3.1.10

---
+++ application/config/eshop_master.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by BYOOS.
+ * 	eshop_master – A Free solution powered by CodeIgniter
+ *  This free software comes from the fusion of several sources including ....
+ * 	Good use
+ * User: Gabriel
+ * Date: 2016/06/18
+ * Time: 10:00 PM
+ * Project: eSHO_master
+ * Website: http://www.byoos.net
+ * modifications  2019/04/23  update PHP7.3  codeignter 3.1.10  BCRYPT and sessions helpers, test on DEBAIN 9.x
+ * BYOOS solutions ltd.  France - Cameroun  gbobard@gmail.com  http://byoos.net   http://byoos.eu
+ */
+
+
+/*
+|--------------------------------------------------------------------------
+| codeigniter 3.1.10  eshop_master  Version  1906b2a
+|--------------------------------------------------------------------------
+|
+*/
+$config['version'] = array (
+  // Manual numbering
+  "major" => '19',
+  "minor" => '06',
+  "patch" => '0',
+  
+  "build" => '2b', // build "b" date 2019/06/20  10:00  gbobard@gmail.com
+);
+
+/*
+|--------------------------------------------------------------------------
+| show or hidden session value into  footer view
+|--------------------------------------------------------------------------
+|
+*/
+$config['show_session_footer'] = false;
+
+$config['enable_health_activity'] = false;
